db6b113802b608981bf25657a462fa77caca4bb2
andresperezfc <felipe3apc@gmail.com>
2023-05-24 10:06:37 -0500

    React redux File Folder Architecture and React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BroserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BroserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BroserRouter>
+    </div>
+  );
 }
 
 export default App;
